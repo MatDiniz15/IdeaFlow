908be84703c54165823b637a94899195a8b4d28c
MatDiniz15 <127975435+MatDiniz15@users.noreply.github.com>
2024-10-08 00:02:07 -0300

    Add files via upload

---
+++ components/CustomTabBar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {  } from "react-native";
+import { style } from "./styles";
+
+
+
+// // export default ({state,navigation})=>{
+//     return (
+        
+
+//     )
+// }
