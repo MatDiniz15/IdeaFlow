2c38bf6e817f94fc6aec041306d4ee3b4c10bc7b
Matheus Diniz <127975435+MatDiniz15@users.noreply.github.com>
2024-10-21 23:03:54 -0300

    Delete global directory

--- global/themes.tsx
+++
@@ -1,9 +0,0 @@
-export const themas = {
-    colors: {
-        primary: '#878af6',
-        secondary: '#ffffff',
-        lightGray: '#d7d8d7',
-        gray: 'black',
-        bgScreen: '#f1f7fa'
-    }
-}
